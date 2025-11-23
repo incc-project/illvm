608a9b56852cf84f32ea39d651abe1fb932d2cae
qaqcatz <qaqcatz@gmail.com>
2025-11-23 21:17:27 +0800

    Disable undefined-internal warning

--- clang/lib/IClang/Driver/CheckDriver.cpp
+++ clang/lib/IClang/Driver/CheckDriver.cpp
@@ -32,7 +32,7 @@ int FuncXCheckDriver::run(
   const int res =
       DriverBase::compile(clangDriver, originalArgv, -1, "", -1, "", -1, "", {},
                           {"-Wno-unused-function", "-Wno-unused-const-variable",
-                           "-Wno-unused-private-field"});
+                           "-Wno-unused-private-field", "-Wno-undefined-internal"});
   DriverBase::fini(global);
   return res;
 }
