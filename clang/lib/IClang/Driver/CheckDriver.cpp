a6e3e10b569b84b5ebca9295245b33ff751ad3a8
qaqcatz <qaqcatz@gmail.com>
2025-11-23 21:36:00 +0800

    Disable unused-parameter, -undefined-inline, unused-but-set-variable warning

--- clang/lib/IClang/Driver/CheckDriver.cpp
+++ clang/lib/IClang/Driver/CheckDriver.cpp
@@ -31,8 +31,10 @@ int FuncXCheckDriver::run(
   assert(global.getIClangMode() == IClangMode::FuncXCheckMode);
   const int res =
       DriverBase::compile(clangDriver, originalArgv, -1, "", -1, "", -1, "", {},
-                          {"-Wno-unused-function", "-Wno-unused-const-variable",
-                           "-Wno-unused-private-field", "-Wno-undefined-internal", "-Wno-unused-variable"});
+      {"-Wno-unused-function", "-Wno-unused-const-variable",
+       "-Wno-unused-private-field", "-Wno-undefined-internal",
+       "-Wno-unused-variable", "-Wno-unused-parameter", "-Wno-undefined-inline",
+       "-Wno-unused-but-set-variable"});
   DriverBase::fini(global);
   return res;
 }
